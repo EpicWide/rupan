9e090bd68e2c366763139a9e7173fcc0cec56b01
EpicWide <dongwon.epic@gmail.com>
2026-06-09 10:02:22 -0500

    Update supabase-browser.ts

--- lib/supabase-browser.ts
+++ lib/supabase-browser.ts
@@ -1,14 +1,11 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl =
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://adpksztfayylowsdjkuu.supabase.co";
 
-if (!supabaseUrl) {
-  throw new Error("Missing NEXT_PUBLIC_SUPABASE_URL");
-}
-
-if (!supabaseAnonKey) {
-  throw new Error("Missing NEXT_PUBLIC_SUPABASE_ANON_KEY");
-}
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkcGtzenRmYXl5bG93c2Rqa3V1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA5OTk3MjUsImV4cCI6MjA5NjU3NTcyNX0.BJNfK65hPfgFXq6fDMwPcrCDF-EnIfLRz-tq8m37gyE";
 
 export const supabaseBrowser = createClient(supabaseUrl, supabaseAnonKey);
